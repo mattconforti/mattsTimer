e4abb0c59626b0fb4e667d5e9648c4d9f96cb64e
Matt Conforti <mattconforti21@gmail.com>
2021-07-01 17:18:51 -0400

    alarm noise upon completion and enter key support

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,15 +1,24 @@
+// define audio variable
+// implemented in every browser? may need a workaround
+let beep_audio = new Audio('beep.mp3');
+
 function start_countdown() {
     var time = parseInt(document.getElementById('time_label').value);
     // if time < 60 sec, reformat! to the way you want it!!
     if (!isNaN(time)) {  // if input is a number - do other checks? 
     // NEED MORE INPUT VALIDATION & do error checking - write tests to error check?
     // note - if "30 check" is input - starts countdown from 30 bc parseInt
-        var myInterval = setInterval(() => {  // do action every x seconds
-            if (time===0) {
-                clearInterval(myInterval);
-                // make some type of noise or alarm??
+        var my_interval = setInterval(() => {  // do action every x seconds
+            if (time === 0) {
+                clearInterval(my_interval);
+                // make alarm play on loop for 10 seconds
+                // mess with volume?? - see macmost video maybe?? 
+                // may be different on windows - need to test on both platforms?
+                beep_audio.loop = true;
+                beep_audio.play();
+                setTimeout(() => beep_audio.loop = false, 10000);
             }
-            else {
+            else {  // time is NOT 0
                 time--;
                 document.getElementById('time_label').value = time;
             }
@@ -21,3 +30,16 @@ function start_countdown() {
 }
 
 document.getElementById('start_button').addEventListener('click', start_countdown);
+
+// make 'enter' key trigger button click
+document.addEventListener('keyup', e => {
+    if (e.key === 'Enter') {
+        e.preventDefault();  // prevent default behavior
+        document.getElementById('start_button').click();  // trigger button click
+    }
+});
+
+// if at any point the input box is clicked into - stop the timer (if it has started!!)!!
+// - user trying to enter a new time!
+
+// branch auto merging? no true master branch? confused
